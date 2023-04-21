44b68e4b8f921da1bc43397fa1f5d110923dc55f
Candace Makeda Moore, MD <31033601+drcandacemakedamoore@users.noreply.github.com>
2023-04-21 21:31:00 +0200

    add a normed and nonnormed slice viewer

--- cvasl/mold.py
+++ cvasl/mold.py
@@ -17,6 +17,51 @@ import matplotlib.pyplot as plt
 import SimpleITK as sitk
 
 
+class NormedSliceViewer:
+    """
+    A class to examine slices of MRIs, or other volumetric data, with normalizaiotn
+
+    """
+    #TODO: should just be an option with widget in slice viewer
+    def __init__(self, volume, figsize=(10, 10)):
+        self.volume = volume
+        self.figsize = figsize
+        self.v = [np.min(volume), np.max(volume)]
+
+        widgets.interact(self.transpose, view=widgets.Dropdown(
+            options=['axial', 'sag', 'cor'],
+            value='axial',
+            description='View:',
+            disabled=False))
+
+    def transpose(self, view):
+        # transpose the image to orient according to the slice plane selection
+        orient = {"sag": [1, 2, 0], "cor": [2, 0, 1], "axial": [0, 1, 2]}
+        self.vol = np.transpose(self.volume, orient[view])
+        maxZ = self.vol.shape[2] - 1
+
+        widgets.interact(
+            self.plot_slice,
+            z=widgets.IntSlider(
+                min=0,
+                max=maxZ,
+                step=1,
+                continuous_update=True,
+                description='Image Slice:'
+            )
+        )
+
+    def plot_slice(self, z):
+        # plot slice for plane which will match the widget intput
+        self.fig = plt.figure(figsize=self.figsize)
+        plt.imshow(
+            self.vol[:, :, z],
+            cmap="gray",
+            vmin=self.v[0],
+            vmax=self.v[1],
+        )
+
+
 class SliceViewer:
     """
     A class to examine slices of MRIs, or other volumetric data
@@ -56,10 +101,9 @@ class SliceViewer:
         plt.imshow(
             self.vol[:, :, z],
             cmap="gray",
-            vmin=self.v[0],
-            vmax=self.v[1],
+            vmin=0,
+            vmax=255,
         )
-
 
 def n4_debias_sitk(
         image_filename,
